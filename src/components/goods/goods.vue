7f6bee499fc16ab9a7efe6acf7ad7bc10b60f2b5
stone <stone.shi@luxsens.com>
2017-09-18 23:23:27 +0800

    右侧滚动

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
         <li v-for="item in goods" class="menu-item">
           <span class="text border-1px">
@@ -9,9 +9,9 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item">
@@ -22,12 +22,10 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span>
-                  <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -39,6 +37,7 @@
 </template>
 
 <script>
+  import BScroll from '../../../node_modules/better-scroll';
   const ERR_OK = 0;
   export default{
     props: {
@@ -48,7 +47,9 @@
     },
     data () {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
     },
     created () {
@@ -57,8 +58,30 @@
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
+          this.$nextTick(() => {
+            this._initBscroll();
+            this._calculateHeight();
+          });
         }
       });
+    },
+    methods: {
+      _initBscroll () {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {});
+      },
+      _calculateHeight () {
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        console.log(foodList);
+        let $height = 0;
+        this.listHeight.push($height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          $height += item.clientHeight;
+          this.listHeight.push($height);
+          console.log(this.listHeight);
+        }
+      }
     }
   };
 </script>
@@ -108,7 +131,6 @@
            font-size :12px
      .foods-wrapper
        flex :1
-       overflow :auto
        .title
          padding-left 14px
          height 26px
@@ -142,8 +164,9 @@
              color :rgb(147,153,159)
            .desc
              margin-bottom :8px
+             line-height :12px
            .extra
-             &.count
+             .count
                margin-right :12px
            .price
              font-weight :700
